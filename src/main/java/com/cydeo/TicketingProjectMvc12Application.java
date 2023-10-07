7f2d1c6835a9e9d3a423d2a884911b4f90165492
halilmek <62161903+halilmek@users.noreply.github.com>
2023-10-07 20:44:16 +0200

    css, js, images copied; and binding java with UI / thymeleaf, entity, dto, service packages created

---
+++ src/main/java/com/cydeo/TicketingProjectMvc12Application.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectMvc12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectMvc12Application.class, args);
+    }
+
+}
